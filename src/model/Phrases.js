8d811fe7f3b76f88041620ff77d7aebeedfccb9a
blockchaingurukerala <65129127+blockchaingurukerala@users.noreply.github.com>
2021-11-23 00:17:18 +0530

    Update Phrases.js

--- src/model/Phrases.js
+++ src/model/Phrases.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://52.56.48.112:27017/taxdb', { useNewUrlParser: true ,useUnifiedTopology: true });
-//mongoose.connect('mongodb+srv://pk:pk@cluster0-yxuce.mongodb.net/onlineexamdb?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://52.56.48.112:27017/taxdb', { useNewUrlParser: true ,useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://pk:pk@cluster0.ozbxe.mongodb.net/taxdb?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true });
 //mongodb+srv://pk:<password>@cluster0-yxuce.mongodb.net/test?retryWrites=true&w=majority
 //mongodb+srv://pk:<password>@cluster0-yxuce.mongodb.net/test?retryWrites=true&w=majority
 
