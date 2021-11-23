e7f5b14adc24c76f0816469f288aeaad9cf68739
blockchaingurukerala <65129127+blockchaingurukerala@users.noreply.github.com>
2021-11-23 09:00:56 +0530

    Update Phrases.js

--- src/model/Phrases.js
+++ src/model/Phrases.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://52.56.48.112:27017/taxdb', { useNewUrlParser: true ,useUnifiedTopology: true });
-try{
 
 
-mongoose.connect('mongodb+srv://pk:pk@cluster0.ozbxe.mongodb.net/taxdb?retryWrites=true&w=majority');
+
+mongoose.connect('mongodb+srv://pk:pk@cluster0.ozbxe.mongodb.net/taxdb?retryWrites=true&w=majority', { useNewUrlParser: true ,useUnifiedTopology: true });
 //mongodb+srv://pk:<password>@cluster0-yxuce.mongodb.net/test?retryWrites=true&w=majority
 //mongodb+srv://pk:<password>@cluster0-yxuce.mongodb.net/test?retryWrites=true&w=majority
-}
-catch(Exception ){
- console.log("connection erro")
-}
+
+
 const Schema = mongoose.Schema;
 var NewUserSchema = new Schema({  
     wallet:String,  
